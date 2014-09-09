c39d0e17a4d9e17f2dd2527f9519a056c83e21ce
Yan Aung <yan.pye.aung@gmail.com>
2014-09-08 19:12:25 -0700

    Add refill calculator post

---
+++ static/js/refill.js
@@ -0,0 +1,18 @@
+function calculate() {
+	var amtCard = document.rcal.amountleft.value;
+	var nRides = document.rcal.numrides.value;
+
+	var x = (50 * nRides - 20 * amtCard) / 21;
+
+	if ( x < 5 ) {
+		x = nRides * 2.50 - amtCard;
+	}
+
+	if ( x < 0 ) {
+		x = 0.00 ;
+	}
+
+	var refill = document.getElementById("refill");
+
+	refill.innerHTML = x.toFixed(2);
+}
